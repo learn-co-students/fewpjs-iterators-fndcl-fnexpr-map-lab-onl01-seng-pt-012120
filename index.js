5953ee92053029df970cc4966ca0a8abb8d5a796
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-28 20:01:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const tutorials = ['what does the this keyword mean?', 'What is the Contutorialuctor OO pattern?', 'implementing Blockchain Web API', 'The Test Driven Development Workflow', 'What is NaN and how Can we Check for it', 'What is the difference between stopPropagation and preventDefault?', 'Immutable State and Pure Functions', 'what is the difference between == and ===?', 'what is the difference between event capturing and bubbling?', 'what is JSONP?'];
 
-const titleCased = tutorials.map(function(tutorial) {
-  return tutorial.toUpperCase();
 
-});
-
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    let tokens = line.split(' ')
+    let capitalizedTokens =
+      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+    let response = capitalizedTokens.join(' ')
+    return response
+  })
+}
