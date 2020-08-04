c48a87577ff4f619ed07abbcacd7c74c8cc49ec3
haileybongo <haileyebongo@gmail.com>
2020-08-03 22:15:26 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+
+  return tutorials.map(function (tutorial){
+    let sentence = tutorial.split(" ");
+    for(let i = 0; i< sentence.length; i++){
+       sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+return sentence.join(" ");
+
+ })}
+  
+
+
+  
