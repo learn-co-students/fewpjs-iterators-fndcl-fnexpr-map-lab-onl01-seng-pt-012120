0f41e6973a4efaf5b250cc081047502b65768815
javers <joeychavira2016@gmail.com>
2020-08-12 15:56:35 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (sentance) {
+    let words = sentance.split(' ');
+    let capitalizedWords = words.map( word => 
+      word.charAt(0).toUpperCase() + word.slice(1) )
+    let response = capitalizedWords.join(' ')
+    return response
+  })
 }
