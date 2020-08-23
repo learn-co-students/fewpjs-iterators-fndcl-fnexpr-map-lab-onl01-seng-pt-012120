ec673573cf43daae828fb0fc3683c65d502ef068
hmcdowell95 <hmcdowell95@github.com>
2020-08-23 14:44:05 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(function(string) {
+    return string.split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  })
